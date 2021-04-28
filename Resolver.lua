96143127ce0eb0eced27a258c514d627802456d5
brendonkuar <79982483+brendonkuar@users.noreply.github.com>
2021-04-28 11:41:41 -0400

    Add files via upload
    Going to update this Lua and fix it. Adding more logic to this resolver will be aids. But I have to make the resolver better so it can actually hit. I also need to make the resolver "smart"

---
+++ Resolver.lua
@@ -0,0 +1,38 @@
+local angles = {
+    [1] = -55,
+    [2] = 55,
+    [3] = 38,
+    [4] = -38,
+    [5] = -29,
+    [6] = 29,
+    [7] = -15,
+    [8] = 15,
+    [9] = 0
+}
+-- good values could be better
+local last_angle = 0
+local new_angle = 0
+local switch1 = false
+local switch2 = false
+local i = 1
+local function resolve(player)
+    plist.set(player, "Correction active", false) -- disable default correction because i have a superiority complex
+    plist.set(player, "Force body yaw", true) -- enable the forcing of the body yaw
+
+    if last_angle == -new_angle and switch1 then
+        new_angle = -angles[i]
+        if switch2 == true then
+            switch1 = not switch1
+        end
+    else
+        if i < #angles then
+            i = i + 1
+        else
+            i = 1
+        end
+        new_angle = angles[i]
+    end
+  plist.set(player, "Force body yaw value", new_angle) -- force yaw value to random
+    last_angle = new_angle
+    switch2 = false
+end
